2b12278600eccdd55862d1809a199f553bd90e11
lolarne <rne.lola@gmail.com>
2021-02-04 14:22:11 +0100

    try to have upload working

--- src/components/galerie/PostForm.jsx
+++ src/components/galerie/PostForm.jsx
@@ -1,51 +1,46 @@
-import './postForm.css';
-import { useEffect , useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './postForm.css';
 import { FETCH } from '../../Fetch.js';
-import FileUpload from '../fileUpload/FileUpload.jsx';
 
 const PostForm = () => {
-    const [Illustration, setIllustration] = useState([]);
+    const [file, setFile] = useState('');
+    const [fileName, setFileName] = useState('');
+    const [uploadedFile, setUploadedFile] = useState({});
+
     const [nameImg, setNameImg] = useState("");
     const [category, setCategory] = useState("");
     const [color, setColor] = useState(0);
     const [date, setDate] = useState("");
     const [material, setMaterial] = useState("");
 
-    const [file, setFile] = useState("");
-    const [fileName, setFileName] = useState("");
-    const [uploadedFile, setUploadedFile] = useState({});
-
-    useEffect(()=>{
-        axios.get(`${FETCH}/galerie`).then((res)=> setIllustration(res.data));
-    }, []);
-
-    const onChange = (e) => {
+    const onChange = e => {
         setFile(e.target.files[0]);
         setFileName(e.target.files[0].name);
-    };
+    }
 
-    const handleUpload = async (e) => {
+    const handleUpload = async e => {
+        e.preventDefault();
         const formData = new FormData();
-        formData.append("file", file);
+        formData.append('file', file)
 
         try {
-            const res = await axios.post(`${FETCH}/upload`, formData, {
+            const res = await axios.post(`http://localhost:8000/api/upload/upload`, formData, {
                 headers: {
-                    "Content-Type": "multipart/form-data",
-                },
+                    'Content-Type': 'multipart/form-data'
+                }
             });
 
-            const { fileName, filePath } = res.data;
+            const { fileName, filePath } = res.data
 
             setUploadedFile({ fileName, filePath });
 
         } catch (err) {
             let message;
-            if (err.response.status === 500) {
-                message = "There was a problem with the server";
+            if (err.reponse.status === 500) {
+                message = 'There was a problem with the server'
             } else {
-                message = "Everything went fine";
+                message = 'Everything went fine'
             }
         }
     };
@@ -69,68 +64,74 @@ const PostForm = () => {
     }
 
     return (
-        <form>
-            <label> Nom :
+        <div className="postForm">
+            <form onSubmit={handleUpload}>
+                <label> Nom :
                 <input type="text" name="name" value={nameImg} onChange={(e) => setNameImg(e.target.value)} required />
-            </label>
-            <label> Catégorie :
-                <select onChange={(e) => setCategory(e.target.value)}>
-                    <option selected value="">...</option>
-                    <option value="landscape">Paysage</option>
-                    <option value="animal">Animaux</option>
-                    <option value="cartoon">Cartoon</option>
-                    <option value="abstract">Abstrait</option>
-                    <option value="mandala">Mandala</option>
-                </select>
-            </label>
-            <label> Matériels :
-                <select onChange={(e) => setMaterial(e.target.value)}>
-                    <option selected value="">...</option>
-                    <option value="crayon">Crayon de couleur</option>
-                    <option value="marker">Feutre</option>
-                    <option value="alcohol">Feutre à alcool</option>
-                    <option value="paint">Peinture</option>
-                    <option value="sew">Couture</option>
-                    <option value="collage">Collage</option>
-                </select>
-            </label>
-            <label>Coloration :</label>
-            <label for="yes">Couleur</label>
-            <input
-                type="radio"
-                name="color"
-                value="1"
-                onChange={(e) => setColor(e.target.value)}
-                required
-            />
-            <label for="no">Noir & Blanc</label>
-            <input
-                type="radio"
-                name="color"
-                value="0"
-                onChange={(e) => setColor(e.target.value)}
-                required
-            />
-            <label> Date:
-                <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} required />
-            </label>
-            <label>Image :
-                <FileUpload
-                className="imageForm"
-                  method={(e) => {
-                    e.preventDefault();
-                    handleUpload();
-                  }}
-                  onChange={(e) => {
-                    onChange(e);
-                  }}
-                  fileName={uploadedFile.fileName}
-                  filePath={uploadedFile.filePath}
-                />
                 </label>
+                <label> Catégorie :
+                <select onChange={(e) => setCategory(e.target.value)}>
+                        <option selected value="">...</option>
+                        <option value="landscape">Paysage</option>
+                        <option value="animal">Animaux</option>
+                        <option value="cartoon">Cartoon</option>
+                        <option value="abstract">Abstrait</option>
+                        <option value="mandala">Mandala</option>
+                    </select>
+                </label>
+                <label> Matériels :
+                <select onChange={(e) => setMaterial(e.target.value)}>
+                        <option selected value="">...</option>
+                        <option value="crayon">Crayon de couleur</option>
+                        <option value="marker">Feutre</option>
+                        <option value="alcohol">Feutre à alcool</option>
+                        <option value="paint">Peinture</option>
+                        <option value="sew">Couture</option>
+                        <option value="collage">Collage</option>
+                    </select>
+                </label>
+                <label>Coloration :</label>
+                <label for="yes">Couleur</label>
+                <input
+                    type="radio"
+                    name="color"
+                    value="1"
+                    onChange={(e) => setColor(e.target.value)}
+                    required
+                />
+                <label for="no">Noir & Blanc</label>
+                <input
+                    type="radio"
+                    name="color"
+                    value="0"
+                    onChange={(e) => setColor(e.target.value)}
+                    required
+                />
+                <label> Date:
+                <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} required />
+                </label>
+                <div>
+                    <input type="file" id="file" onChange={onChange} />
+                    <label htmlFor="file"></label>
+                </div>
+                <input type="submit" value="Télécharger" />
+            </form>
+            {uploadedFile ? (
+                <div>
+                    <img src={uploadedFile.filePath} alt="" />
+                    <p>{uploadedFile.fileName}</p>
+                </div>
+            )
+                : null}
             <button type="submit" value="submit" onClick={handleSubmit}>Envoyer</button>
-        </form>
-    );
+
+        </div>
+    )
+
+
+
+
+
 }
 
 export default PostForm;
