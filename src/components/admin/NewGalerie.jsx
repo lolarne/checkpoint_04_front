f9b6b89be93f79c545bd8930cd35dee5f7e15288
lolarne <rne.lola@gmail.com>
2021-02-04 17:11:07 +0100

    upload ok + css

--- src/components/admin/NewGalerie.jsx
+++ src/components/admin/NewGalerie.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { FETCH } from '../../Fetch.js';
+import './newGalerie.css';
 
 const NewGalerie = () => {
     const [illustration, setIllustration] = useState([]);
@@ -20,12 +21,22 @@ const NewGalerie = () => {
     }
 
     return (
-        <div className="galeryCard">
+        <div className="NewgaleryCard">
             {illustration.map((item) => (
-                <div>
-                    <p>{item.name}{item.category}</p>
-                    <button type="button" onClick={(e) => handleAccept(e, item.id)}>Ajouter</button>
-                    <button type="button" onClick={(e) => handleDelete(e, item.id)}>Supprimer</button>
+                <div className="card">
+                    <div>
+                        <div className="img"><img src={item.img} /></div>
+                        <p>Titre : {item.name}</p>
+                        <p>Catégorie : {item.category}</p>
+                        <p>Matériel : {item.material}</p>
+                        <p>Couleur (1 = oui; 0 = Noir & Blanc) : {item.color}</p>
+                        <p>Date : {item.date_creation}</p>
+                        
+                    </div>
+                    <div className="btn">
+                        <button type="button" onClick={(e) => handleAccept(e, item.id)}>Ajouter</button>
+                        <button type="button" onClick={(e) => handleDelete(e, item.id)}>Supprimer</button>
+                    </div>
                 </div>
             ))}
         </div>
